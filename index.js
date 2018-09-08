0e8a852d92d0e3dcbe40fdf3d7b6d6b6be0439e2
ЮЛЯ-ПК\ЮЛЯ <julia.avsyuk@mail.ru>
2018-09-08 16:55:28 +0300

    add commits

--- index.js
+++ index.js
@@ -6,17 +6,23 @@ let input = document.getElementById('input'),
     clear = document.getElementById('clear'),
     resultDisplayed = false; //flag to keep an eye on what output is displayed
 
+//add click handlers to number buttons
 for(let i=0; i<number.length;i++){
     number[i].addEventListener("click", function (e) {
+        // storing current input string and its last character in variables - used later
         let currentString = input.innerHTML;
         let lastChar = currentString[currentString.length - 1];
-
+        // if result is not diplayed, just keep adding
         if(resultDisplayed === false){
             input.innerHTML += e.target.innerHTML;
         } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/"){
+            // if result is currently displayed and user pressed an operator
+            // we need to keep on adding to the string for next operation
             resultDisplayed = false;
             input.innerHTML += e.target.innerHTML;
         } else {
+            // if result is currently displayed and user pressed a number
+            // we need clear the input string and add the new input to start the new opration
             resultDisplayed = false;
             input.innerHTML = "";
             input.innerHTML = e.target.innerHTML;
@@ -24,32 +30,44 @@ for(let i=0; i<number.length;i++){
 
     });
 }
-
+// adding click handlers to operator buttons
 for(let i=0; i<operator.length; i++){
     operator[i].addEventListener("click", function (e) {
+        // storing current input string and its last character in variables - used later
         let currentString = input.innerHTML;
         let lastChar = currentString[currentString.length - 1];
-
+        // if last character entered is an operator, replace it with the currently pressed one
         if(lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/"){
             let newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
             input.innerHTML = newString;
         } else if (currentString.length == 0){
+            // if first key pressed is an opearator, don't do anything
             console.log("Enter a number first");
         } else {
+            // else just add the operator pressed to the input
             input.innerHTML += e.target.innerHTML;
         }
 
     });
 }
-
+// on click of 'equal' button
 result.addEventListener("click", function () {
+    // this is the string that we will be processing eg. -10+26+33-56*34/23
     let inputString = input.innerHTML;
+    // forming an array of numbers
     let numbers = inputString.split(/\+|\-|\×|\÷/g);
+    // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
+    // first we replace all the numbers and dot with empty string and then split
     let operators = inputString.replace(/[0-9]|\./g, "").split("");
     console.log(inputString);
     console.log(operators);
     console.log(numbers);
     console.log("-------------------------");
+
+    // now we are looping through the array and doing one operation at a time.
+    // first divide, then multiply, then subtraction and then addition
+    // as we move we are alterning the original numbers and operators array
+    // the final element remaining in the array will be the output
 
     let divide = operators.indexOf("÷");
     while(divide != -1) {
@@ -74,12 +92,14 @@ result.addEventListener("click", function () {
 
     let add = operators.indexOf("+");
     while(add != -1) {
+        // using parseFloat is necessary, otherwise it will result in string concatenation
         numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
         operators.splice(add, 1);
         add = operators.indexOf("+");
     }
+    // displaying the output
     input.innerHTML = numbers[0];
-
+    // turning flag if result is displayed
     resultDisplayed = true;
 });
 
